c9556cf259345a3fdfaf19ba08f744ed46c2d3a1
Gonzague de Carpentier <decarpentierg@gmail.com>
2023-01-18 19:49:19 +0100

    modify main.py to enable parallelization on several machines

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from time import time
 from tqdm import tqdm
 import os
 import pickle
+import argparse
 
 import patchmatch as pm
+
+parser = argparse.ArgumentParser(description="Run PatchMatch on test database")
+parser.add_argument("indices", type=str, help="Run on images TP_C%1_%2 to TP_C%1_%3 where argument=%1,%2,%3")
+args = parser.parse_args()
+c_idx, start, stop = [int(x) for x in args.indices.split(",")]
 
 DATA = "CMFDdb_grip/forged_images/"  # directory where to find forged images
 RESULTS = "results/"  # directory where to find the results
@@ -15,7 +21,12 @@ ls = sorted([x[:10] for x in os.listdir(DATA) if "copy" in x])
 zernike_times = {}
 patchmatch_times = {}
 
-for im_name in tqdm(ls):
+for idx in tqdm(range(start, stop)):
+    # Compute image name
+    im_name = f"TP_C{c_idx:02d}_{idx:03d}"
+    if im_name not in ls:  # if image does not exist, simply skip it
+        continue
+
     # Load image
     im = Image.open(f"{DATA}/{im_name}_copy.png")
     im = np.array(im).astype("double")
@@ -44,8 +55,8 @@ for im_name in tqdm(ls):
     res = {attribute[0]:a.__getattribute__(attribute[0]) for attribute in pm.spec[1:]}
     np.savez_compressed(f"{RESULTS}/{im_name}_results.npz", **res)
 
-with open(f"{RESULTS}/zernike_times.pkl", "wb") as file:
+with open(f"{RESULTS}/zernike_times_C{c_idx:02d}_{start:03d}_to_{stop:03d}.pkl", "wb") as file:
     pickle.dump(zernike_times, file)
 
-with open(f"{RESULTS}/patchmatch_times.pkl", "wb") as file:
+with open(f"{RESULTS}/patchmatch_times_C{c_idx:02d}_{start:03d}_to_{stop:03d}.pkl", "wb") as file:
     pickle.dump(patchmatch_times, file)
